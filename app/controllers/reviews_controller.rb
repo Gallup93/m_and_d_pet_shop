91b805c0d5b89a5442346bc67cc269102db64690
Michael Gallup <Gallup93@icloud.com>
2020-05-11 22:16:03 -0600

    Add create action to reviews controller, fix typo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,7 @@ class ReviewsController < ApplicationController
   def new
     @shelter = Shelter.find(params[:id])
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,5 @@ Rails.application.routes.draw do
 
   #reviews
   get 'shelters/:id/reviews/new', to: 'reviews#new'
-  post 'shelters/:id/reviews/new', to: 'review#create'
+  post 'shelters/:id/reviews/new', to: 'reviews#create'
 end
