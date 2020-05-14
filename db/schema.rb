cb9c06de86dcdbaa127b916b4962f4e53e8142d6
Michael Gallup <Gallup93@icloud.com>
2020-05-14 16:35:23 -0600

    Add adoption model spec, Add adoptions table to database, Add model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200511232557) do
+ActiveRecord::Schema.define(version: 20200514222727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adoptions", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.integer "phone"
+    t.string "description"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
