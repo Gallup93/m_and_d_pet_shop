152fd51a9baf768ab202253e31a9eb7df6f45156
Michael Gallup <Gallup93@icloud.com>
2020-05-14 12:25:27 -0600

    Commented out possibly unused code

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   #reviews
   get '/shelters/:id/reviews/new', to: 'reviews#new'
-  get '/shelters/:id/reviews', to: 'reviews#new'
+  # get '/shelters/:id/reviews', to: 'reviews#new'
   get 'shelters/:id/:review_id/edit', to: 'reviews#edit'
   get '/shelters/:id/:review_id/delete', to: 'reviews#destroy'
   post '/shelters/:id/reviews', to: 'reviews#create'
