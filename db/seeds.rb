6b4b4603a8d3206cf4786d299f86c3f0e0b4ffb7
Daniel Selph <daniel.p.selph@gmail.com>
2020-05-19 11:16:48 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Shelter.destroy_all
+Pet.destroy_all
+Adoptions.destroy_all
+AdoptionPets.destroy_all
+
 shelter1 = Shelter.create(name: "Randy's Rodent Ranch", address: "555 Hamster Ave", city: "Richmond", state: "VA", zip: "12345")
 shelter2 = Shelter.create(name: "Turkey Paradise", address: "876 Sandwich Lane", city: "Cheyenne", state: "WY", zip: "23456")
 shelter3 = Shelter.create(name: "Lots O Lizards", address: "2987 Big Pond Pkwy", city: "Jacksonville", state: "FL", zip: "14356")
