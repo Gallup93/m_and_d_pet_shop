6d9b45a09877c320878018d2111737a8f8d6e2ee
Michael Gallup <Gallup93@icloud.com>
2020-05-19 11:08:38 -0600

    Made progress user story 18. Passing driver off to dan

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -7,10 +7,14 @@ class AdoptionsController < ApplicationController
   # end
 
   def create
-    application = Adoption.create(adoption_params)
+    application = Adoption.create!(adoption_params)
 
     if application.save && params[:check_box] != nil
       fav_ids = params[:check_box]
+      fav_ids.each do |pet_id|
+        pet = Pet.find(pet_id.to_i)
+        AdoptionPet.create(pet_id: pet.id, adoption_id: application.id)
+      end
       fav_ids.each{|fav| favorites.delete_fav_pet(fav.to_i)}
       flash[:notice] = "Application submitted!"
       redirect_to '/favorites'
